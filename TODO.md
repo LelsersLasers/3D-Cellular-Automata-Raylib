9ebbee05236060e3991b71fe39b6e2bddd98fd16
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-06-06 14:53:43 -0700

    Adding the actual ceullular automata

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
 # TODO
 
 - The actual 3d cellular automata
+    - Does it work?
 - Replace set fps with delta time
     - Use same system from previous project
+- Full screen/resizable window
+- On demand changes for bound and cell rules
+- Menu screens etc?
--- main.cpp
+++ main.cpp
@@ -34,9 +34,9 @@ enum NeightborType {
 };
 
 
-int SURVIVAL[] = { 6, 7, 8 };
-int SPAWN[] = { 6, 7, 8 };
-int STATE[] = { 3 };
+int SURVIVAL[] = { 4 };
+int SPAWN[] = { 4 };
+int STATE = 5;
 NeightborType NEIGHBOR = MOORE;
 
 
@@ -58,27 +58,30 @@ public:
     int hp;
     Vector3 pos; // center of cube
     float s; // size
-    Color color;
     int neighbors = 0;
 
     Cell() {} // default
 
-    Cell(State state, int hp, Vector3 pos, float s, Color color) {
+    Cell(State state, int hp, Vector3 pos, float s) {
         this->state = state;
         this->hp = hp;
         this->pos = pos;
         this->s = s;
-        this->color = color;
     }
 
     void draw() {
         if (this->state != DEAD) {
-            DrawCube(this->pos, this->s, this->s, this->s, this->color);
+            Color color = RED;
+            if (this->state == DYING) {
+                int brightness = (int) ((float)this->hp/STATE * 255.0f);
+                color = (Color){ brightness, brightness, brightness, 255 };
+            }
+            DrawCube(this->pos, this->s, this->s, this->s, color);
         }
     }
     void draw(Color color) {
+        this->draw();
         if (this->state != DEAD) {
-            DrawCube(this->pos, this->s, this->s, this->s, this->color);
             DrawCubeWires(this->pos, this->s, this->s, this->s, color);
         }
     }
@@ -99,7 +102,7 @@ int main(void) {
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "3D Cellular Automata");
-    SetTargetFPS(60);
+    SetTargetFPS(3);
 
     Camera3D camera = { 0 };
     camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
@@ -110,9 +113,9 @@ int main(void) {
 
     float cameraLat = 20.0f;
     float cameraLon = 20.0f;
-    float cameraRadius = 20.0f;
-    float cameraMoveSpeed = 1.0f;
-    float cameraZoomSpeed = 0.25f;
+    float cameraRadius = 30.0f;
+    float cameraMoveSpeed = 1.0f * 20.0f;
+    float cameraZoomSpeed = 0.25f * 20.0f;
 
 
     Cell cells[CELL_BOUNDS][CELL_BOUNDS][CELL_BOUNDS];
@@ -120,14 +123,13 @@ int main(void) {
         for (int y = 0; y < CELL_BOUNDS; y++) {
             for (int z = 0; z < CELL_BOUNDS; z++) {
                 cells[x][y][z].state = (double)rand()/(double)RAND_MAX < 0.2 ? State::ALIVE : State::DEAD;
-                cells[x][y][z].hp = 5;
+                cells[x][y][z].hp = STATE;
                 cells[x][y][z].pos = (Vector3){
                     CELL_SIZE * (x - (CELL_BOUNDS - 1.0f) / 2.0f),
                     CELL_SIZE * (y - (CELL_BOUNDS - 1.0f) / 2.0f),
                     CELL_SIZE * (z - (CELL_BOUNDS - 1.0f) / 2.0f)
                 };
                 cells[x][y][z].s = CELL_SIZE;
-                cells[x][y][z].color = (Color){ 255, 0, 0, 255 };
             }
         }
     }
@@ -158,16 +160,16 @@ int main(void) {
             ClearBackground(RAYWHITE);
 
             BeginMode3D(camera);
-
-                for (int x = 0; x < CELL_BOUNDS; x++) {
-                    for (int y = 0; y < CELL_BOUNDS; y++) {
-                        for (int z = 0; z < CELL_BOUNDS; z++) {
-                            cells[x][y][z].draw(GREEN);
-                        }
+            
+            for (int x = 0; x < CELL_BOUNDS; x++) {
+                for (int y = 0; y < CELL_BOUNDS; y++) {
+                    for (int z = 0; z < CELL_BOUNDS; z++) {
+                        cells[x][y][z].draw(GREEN);
                     }
                 }
+            }
 
-                DrawCubeWires((Vector3){ 0.0f, 0.0f, 0.0f }, 2.0f, 2.0f, 2.0f, BLUE);
+            DrawCubeWires((Vector3){ 0, 0, 0 }, CELL_SIZE * CELL_BOUNDS, CELL_SIZE * CELL_BOUNDS, CELL_SIZE * CELL_BOUNDS, BLUE);
 
             EndMode3D();
 
@@ -183,27 +185,65 @@ int main(void) {
 
         EndDrawing();
 
-        // for (int x = 0; x < CELL_BOUNDS; x++) {
-        //     for (int y = 0; y < CELL_BOUNDS; y++) {
-        //         cells[x][y].neighbors = 0;
-        //         int offsets[8][2] = {{1, 0}, {1, 1}, {1, -1}, {0, 1}, {0, -1}, {-1, 0}, {-1, 1}, {-1, -1}};
-        //         for (int i = 0; i < 8; i++) {
-        //             if (x + offsets[i][0] >= 0 && x + offsets[i][0] < CELL_BOUNDS && y + offsets[i][1] >= 0 && y + offsets[i][1] < CELL_BOUNDS) {
-        //                 cells[x][y].neighbors += cells[x + offsets[i][0]][y + offsets[i][1]].state == State::ALIVE ? 1 : 0;
-        //             }
-        //         }
-        //     }
-        // }
-        // for (int x = 0; x < CELL_BOUNDS; x++) {
-        //     for (int y = 0; y < CELL_BOUNDS; y++) {
-        //         if (cells[x][y].neighbors == 3) {
-        //             cells[x][y].state = ALIVE;
-        //         }
-        //         else if (cells[x][y].neighbors < 2 || cells[x][y].neighbors > 3) {
-        //             cells[x][y].state = DEAD;
-        //         }
-        //     }
-        // }
+        int offset_options[] = { -1, 0, 1 };
+        for (int x = 0; x < CELL_BOUNDS; x++) {
+            for (int y = 0; y < CELL_BOUNDS; y++) {
+                for (int z = 0; z < CELL_BOUNDS; z++) {
+                    cells[x][y][z].neighbors = 0;
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            for (int k = 0; k < 3; k++) {
+                                bool sameCell = i == 0 && j == 0 && k == 0;
+                                if (!sameCell &&
+                                    x + offset_options[i] >= 0 && x + offset_options[i] < CELL_BOUNDS &&
+                                    y + offset_options[j] >= 0 && y + offset_options[j] < CELL_BOUNDS &&
+                                    z + offset_options[k] >= 0 && z + offset_options[k] < CELL_BOUNDS) {
+                                    cells[x][y][z].neighbors += cells[x + offset_options[i]][y + offset_options[j]][z + offset_options[k]].state == State::ALIVE ? 1 : 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int x = 0; x < CELL_BOUNDS; x++) {
+            for (int y = 0; y < CELL_BOUNDS; y++) {
+                for (int z = 0; z < CELL_BOUNDS; z++) {
+                    switch (cells[x][y][z].state) {
+                        case ALIVE: {
+                            bool willDie = true;
+                            for (int value : SURVIVAL) {
+                                if (cells[x][y][z].neighbors == value) {
+                                    willDie = false;
+                                    break;
+                                }
+                            }
+                            if (willDie) {
+                                cells[x][y][z].state = DYING;
+                            }
+                            break;
+                        }
+                        case DEAD: {
+                            for (int value : SPAWN) {
+                                if (cells[x][y][z].neighbors == value) {
+                                    cells[x][y][z].state = ALIVE;
+                                    cells[x][y][z].hp = STATE;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        case DYING: {
+                            cells[x][y][z].hp--;
+                            if (cells[x][y][z].hp == 0) {
+                                cells[x][y][z].state = DEAD;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     CloseWindow();        // Close window and OpenGL context
